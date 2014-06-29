1dd525706cff7c31837c33195dff37891f9d46aa
AlisamfP <eatliightning@gmail.com>
2014-06-29 16:26:50 -0700

    got json decoded

--- get_using_request.py
+++ get_using_request.py
@@ -1,8 +1,7 @@
-import requests, simplejson
+import requests
+import json
 
 r = requests.get("http://ws.audioscrobbler.com/2.0/?method=user.gettopartists&user=alisatrocity&api_key=8148fb40ef9511752203d2c4591e63d0&format=json&limit=5")
 print r.status_code
-info = r.text
+info = json.loads(r.text)
 print info
-         
-
