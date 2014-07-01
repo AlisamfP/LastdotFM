1c488aaf684d9195ba8c733d62b421ef7ccd2f5b
AlisamfP <eatliightning@gmail.com>
2014-06-30 17:33:05 -0700

    added play count

--- get_using_request.py
+++ get_using_request.py
@@ -1,12 +1,11 @@
-import requests
-import simplejson
+import sys, requests, simplejson
 
 r = requests.get("http://ws.audioscrobbler.com/2.0/?method=user.gettopartists&user=alisatrocity&api_key=8148fb40ef9511752203d2c4591e63d0&format=json&limit=5")
 info = simplejson.loads(r.text)
 def get_artists(data):
     index = 1
     for item in data["topartists"]["artist"]:
-        print "%s: %s" % (index, item["name"])
+        print "%s: %s\nPlay count: %s\n" % (index, item["name"], item["playcount"])
         index+=1
 
 print "User: %s\n" % (info["topartists"]["@attr"]["user"])
