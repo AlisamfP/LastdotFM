227bb2d60ceb94e5af534ba1215aaff3f1ba66e6
AlisamfP <eatliightning@gmail.com>
2014-07-04 11:59:37 -0700

    IT FUCKING WORKS

--- data.js
+++ data.js
@@ -1,12 +1,15 @@
 var lastFMurl = 'http://ws.audioscrobbler.com/2.0/?method=user.gettopartists&user=alisatrocity&api_key=8148fb40ef9511752203d2c4591e63d0&format=json&limit=10';
-
-var data = function makeRequest(apiUrl){
+function makeRequest(apiUrl){
 	$.ajax({
 		url: apiUrl,
 	})
 	.done(function(res) {
-		console.log("success");
-		return playCountData(res['topartists']['artist']);
+		d3.select(".chart")
+  			.selectAll("div")
+    		.data(playCountData(res['topartists']['artist']))
+  		.enter().append("div")
+    		.style("width", function(d) { return d + "px"; })
+    		.text(function(d) { return d; });
 	})
 	.fail(function() {
 		console.log("error");
@@ -17,22 +20,13 @@ var data = function makeRequest(apiUrl){
 }
 
 function playCountData(array){
+	var x = [];
 	for (item in array){
-		console.log(array[item]['name'] + " : " + array[item]['playcount']);
-		// return (array[item]['playcount']);
-		// return (data[item]['playcount'])
+		x.push(parseInt(array[item]['playcount']));
+		// console.log(array[item]['name'] + " : " + array[item]['playcount']);
 	}
+	console.log(x);
+	return x;
 }
 
-
-// var data = [1, 2, 99, 32, 42, 100];
-// var x = d3.scale.linear()
-//     .domain([0, d3.max(data)])
-//     .range([0, 420]);
-
-d3.select(".chart")
-  .selectAll("div")
-    .data(data(lastFMurl))
-  .enter().append("div")
-    .style("width", function(d) { return d + "px"; })
-    .text(function(d) { return d; });
+makeRequest(lastFMurl);
