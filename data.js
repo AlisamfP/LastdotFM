e3a11f955d4c6bff90ef22c6fd15b113052e2126
AlisamfP <eatliightning@gmail.com>
2014-07-03 18:56:56 -0700

    returns array of top artists

--- data.js
+++ data.js
@@ -1,20 +1,22 @@
+var lastFMurl = 'http://ws.audioscrobbler.com/2.0/?method=user.gettopartists&user=alisatrocity&api_key=8148fb40ef9511752203d2c4591e63d0&format=json&limit=10';
+
 var data = function makeRequest(apiUrl){
 	$.ajax({
 		url: apiUrl,
 	})
 	.done(function(res) {
 		console.log("success");
-		console.log(res);
+		console.log(res['topartists']['artist']);
 	})
 	.fail(function() {
 		console.log("error");
 	})
 	.always(function() {
 		console.log("complete");
-	});
-	
+	});	
 }
 
+data(lastFMurl)
 
 
 var x = d3.scale.linear()
@@ -23,7 +25,7 @@ var x = d3.scale.linear()
 
 d3.select(".chart")
   .selectAll("div")
-    .data(data('http://ws.audioscrobbler.com/2.0/?method=user.gettopartists&user=alisatrocity&api_key=8148fb40ef9511752203d2c4591e63d0&format=json'))
+    .data()
   .enter().append("div")
     .style("width", function(d) { return x(d) + "px"; })
     .text(function(d) { return d; });
