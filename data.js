1febf3c8673c841d29c08d1cc45510aeb9c657c7
AlisamfP <eatliightning@gmail.com>
2014-07-06 03:08:51 -0700

    can enter last.fm username to generate chart

--- data.js
+++ data.js
@@ -1,11 +1,16 @@
+$('document').ready(function(){
 $('button').click(function(event) {
 	$('div').empty();
-	makeURL((document.getElementById('period').value), (document.getElementById('limit').value));
+	makeURL((document.getElementById('user').value),
+		(document.getElementById('period').value), 
+		(document.getElementById('limit').value));
 });
 
-function makeURL(period, limit){
-	limit = (limit != undefined) ? limit : '10';
-	makeRequest('http://ws.audioscrobbler.com/2.0/?method=user.gettopartists&user=alisatrocity&api_key=8148fb40ef9511752203d2c4591e63d0&format=json&period='+period+'&limit='+limit);
+function makeURL(user, period, limit){
+	limit = (isNaN(limit) != true) ? limit : 10;
+	url = 'http://ws.audioscrobbler.com/2.0/?method=user.gettopartists&user=';
+	api_key = '8148fb40ef9511752203d2c4591e63d0';
+	makeRequest(url+user+'&api_key='+api_key+'&format=json&period='+period+'&limit='+limit);
 }
 
 function makeRequest(apiUrl){
@@ -20,8 +25,8 @@ function makeRequest(apiUrl){
     		.style("width", function(d) { return d[1] + "px"; })
     		.text(function(d) { return d[0]+" : "+d[1]; });
 	})
-	.fail(function() {
-		console.log("error");
+	.fail(function(err) {
+		console.log("error: ", err);
 	})
 	.always(function() {
 		console.log("complete");
@@ -29,11 +34,10 @@ function makeRequest(apiUrl){
 }
 
 function playCountData(array){
-	var x = [];
+	var artists = [];
 	for (item in array){
-		x.push([array[item]['name'], parseInt(array[item]['playcount'])]);
+		artists.push([array[item]['name'], parseInt(array[item]['playcount'])]);
 	}
-	return x;
+	return artists;
 }
-
-makeURL('alisatrocity');
+});
