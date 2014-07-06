1228ac7c417b8e1fe8cb4ffd9e8bce816e6b6368
AlisamfP <eatliightning@gmail.com>
2014-07-06 02:36:47 -0700

    added names for input and removed commented out shit

--- data.js
+++ data.js
@@ -1,12 +1,10 @@
 $('button').click(function(event) {
 	$('div').empty();
-	// console.log(document.getElementById('period').value);
 	makeURL((document.getElementById('period').value), (document.getElementById('limit').value));
 });
 
 function makeURL(period, limit){
 	limit = (limit != undefined) ? limit : '10';
-	console.log(period, limit);
 	makeRequest('http://ws.audioscrobbler.com/2.0/?method=user.gettopartists&user=alisatrocity&api_key=8148fb40ef9511752203d2c4591e63d0&format=json&period='+period+'&limit='+limit);
 }
 
@@ -34,7 +32,6 @@ function playCountData(array){
 	var x = [];
 	for (item in array){
 		x.push([array[item]['name'], parseInt(array[item]['playcount'])]);
-		// console.log(array[item]['name'] + " : " + array[item]['playcount']);
 	}
 	return x;
 }
