92cd7576c7434467df5bca38d1e78102e9dfed06
AlisamfP <eatliightning@gmail.com>
2014-07-07 00:50:31 -0700

    commented shit

--- visual.js
+++ visual.js
@@ -1,13 +1,14 @@
 function generateChart(info){
 	//https://github.com/mbostock/d3/wiki/Quantitative-Scales
+  // create array of only artist playcounts
 	var dataLength = info.map(function(n){
 			return n[1];
 		});
-  console.log(dataLength);
 	var width = 960, height = 500;
+  // variable to run scaling funtion on variables
 	var y = d3.scale.linear()
-		.domain([0, d3.max(dataLength)])
-		.range([height, 0]);
+		.domain([0, d3.max(dataLength)])  //range of data
+		.range([height, 0]);              //range to scale to
 	var chart = d3.select(".chart")
     .attr('width', width)
     .attr('height', height);
@@ -16,15 +17,15 @@ function generateChart(info){
   	.data(info)
   	.enter().append("g")
     	.attr('transform', function(d, i){
-  	    	return "translate(" + i * barWidth + ",0)";
+  	    	return "translate(" + i * barWidth + ",0)";   //spread bars throughout page
   	});
   bar.append("rect")
     .attr("y", function(d) { return y(d[1]); })
-  	.attr('height', function(d){return height - y(d[1]);})
+  	.attr('height', function(d){ return height - y(d[1]); })
   	.attr('width', barWidth - 1);
   bar.append("text")
-  	.attr('x', barWidth/2)
-  	.attr("y", function(d){ return y(d[1]); })
+  	.attr('x', barWidth / 2)
+  	.attr("y", function(d){ return y(d[1]) + 15; })
   	.attr('dy', '.75em')
-  	.text(function(d){return d[0]});
+  	.text(function(d){ return d[0] });
 };
