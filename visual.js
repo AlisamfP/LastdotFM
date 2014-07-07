efaceb9d6f45975706a31ed24418d28f928396e5
AlisamfP <eatliightning@gmail.com>
2014-07-07 00:44:40 -0700

    it looks like a fucking bar chart now bitches

--- visual.js
+++ visual.js
@@ -3,25 +3,28 @@ function generateChart(info){
 	var dataLength = info.map(function(n){
 			return n[1];
 		});
-	var barHeight = 20;
-	var x = d3.scale.linear()
+  console.log(dataLength);
+	var width = 960, height = 500;
+	var y = d3.scale.linear()
 		.domain([0, d3.max(dataLength)])
-		.range([0, 800]);
+		.range([height, 0]);
 	var chart = d3.select(".chart")
-			.attr('width', 800)
-    	.attr('height', barHeight * dataLength.length);
-  var	bar = chart.selectAll("g")
-  		  .data(info)
-  		.enter().append("g")
-  			.attr('transform', function(d, i){
-  				return "translate(0," + i * barHeight + ")";
-  			});
-  	bar.append("rect")
-  		.attr('width', function(d){return x(d[1]);})
-  		.attr('height', barHeight - 1);
-  	bar.append("text")
-  		.attr('x', function(d){return x(d[1])-3;})
-  		.attr("y", barHeight / 2)
-  		.attr('dy', '.35em')
-  		.text(function(d){return d[0]});
-}
+    .attr('width', width)
+    .attr('height', height);
+  var barWidth = width / info.length
+  var bar = chart.selectAll("g")
+  	.data(info)
+  	.enter().append("g")
+    	.attr('transform', function(d, i){
+  	    	return "translate(" + i * barWidth + ",0)";
+  	});
+  bar.append("rect")
+    .attr("y", function(d) { return y(d[1]); })
+  	.attr('height', function(d){return height - y(d[1]);})
+  	.attr('width', barWidth - 1);
+  bar.append("text")
+  	.attr('x', barWidth/2)
+  	.attr("y", function(d){ return y(d[1]); })
+  	.attr('dy', '.75em')
+  	.text(function(d){return d[0]});
+};
