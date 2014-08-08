cfbf28a98e4b74e50fe7cf31467f5bd5e538a0b8
AlisamfP <eatliightning@gmail.com>
2014-08-08 16:43:13 -0700

    app shows index.html now

--- web.js
+++ web.js
@@ -1,13 +1,14 @@
 // web.js
 var express = require("express");
 var logfmt = require("logfmt");
+var fs = require("fs");
 var app = express();
 
 app.use(logfmt.requestLogger());
 
 app.get('/', function(req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end(index);
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.end(fs.readFileSync('index.html'));
 });
 
 var port = Number(process.env.PORT || 5000);
