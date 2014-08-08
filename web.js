552c39f84bcc54b8e57791280caccb07fa91cd8b
AlisamfP <eatliightning@gmail.com>
2014-08-08 16:39:08 -0700

    changed to show index I think?

--- web.js
+++ web.js
@@ -6,7 +6,8 @@ var app = express();
 app.use(logfmt.requestLogger());
 
 app.get('/', function(req, res) {
-  res.send('Hello World!');
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end(index);
 });
 
 var port = Number(process.env.PORT || 5000);
