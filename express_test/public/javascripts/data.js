4e70ee8df10481d3da5b35c534053f923f8bfca3
AlisamfP <eatliightning@gmail.com>
2014-08-09 16:12:11 -0700

    added express and jade

---
+++ express_test/public/javascripts/data.js
@@ -0,0 +1,45 @@
+$('document').ready(function(){
+	$('button').click(function() {
+		//clear chart before making new one
+		$('svg').empty();
+		return makeURL((document.getElementById('user').value),
+			(document.getElementById('period').value),
+			(document.getElementById('limit').value));
+	});
+	function makeURL(user, period, limit){
+		limit = (isNaN(limit) !== true) ? parseInt(limit) : 10;
+		baseUrl = 'http://ws.audioscrobbler.com/2.0/?method=user.gettopartists&user=';
+		apiKey = '8148fb40ef9511752203d2c4591e63d0';
+		makeRequest(baseUrl+user
+			+'&api_key='+apiKey+
+			'&format=json&period='+period+
+			'&limit='+limit);
+	}
+	function makeRequest(apiUrl){
+		$.ajax({
+			url: apiUrl,
+		})
+		.done(function(res) {
+			playCountData(res['topartists']['artist']);
+		})
+		.fail(function(err) {
+			console.log('error: ', err);
+		})
+		.always(function() {
+			console.log('complete');
+			$('.bar').click(function(){
+				var active = '.barText' + this.id.toString();
+				$(active).removeAttr('visibility');
+			});
+		});
+	}
+
+	function playCountData(array){
+		var artists = [];
+		for (item in array){
+			artists.push([array[item]['name'], parseInt(array[item]['playcount'])]);
+		}
+		return generateChart(artists);
+		// return createCircles(artists);
+	}
+});
