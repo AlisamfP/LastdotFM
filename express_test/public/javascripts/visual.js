63cf794c251a4b823a25f6ca4918f0a55e4f5bd8
AlisamfP <eatliightning@gmail.com>
2014-08-10 14:37:06 -0700

    updated styling and refactored a bit

--- express_test/public/javascripts/visual.js
+++ express_test/public/javascripts/visual.js
@@ -1,39 +1,42 @@
 function generateChart(info){
   var margin = {top: 20, right: 30, bottom: 30, left: 40},
-    width = 960 - margin.left - margin.right,
-    height = 500 - margin.top - margin.bottom;
+      width  = 960 - margin.left - margin.right,
+      height = 500 - margin.top - margin.bottom;
+
   var artists = info.map(function(n){ return n[0]; });
   var x = d3.scale.ordinal()
-    .rangeRoundBands([0, width], .1);
+            .rangeRoundBands([0, width], .2);
+
   //linear scaling variable.
     // domain == range of data.
     // range == desired height/width of chart.
   var y = d3.scale.linear()
     .range([height, 0]);
+
   //set the y axis to the left side
   var yAxis = d3.svg.axis()
-      .scale(y)
-      .orient('left');
-  var chart = d3.select('.chart')
-    .attr('width', width + margin.left + margin.right)
-    .attr('height', height + margin.top + margin.bottom)
-    .append('g')
-    .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
+                .scale(y)
+                .orient('left');
 
-  x.domain(info.map(function(n){ return n[0];}));
+  var chart = d3.select('.chart')
+                .attr('width', width + margin.left + margin.right)
+                .attr('height', height + margin.top + margin.bottom)
+                .append('g')
+                .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
+
+  x.domain(artists);
   y.domain([0, d3.max(info.map(function(n){ return n[1];}))]);
 
-var bar =
-  chart.selectAll('.bar')
-    .data(info)
-      .enter()
-        .append('rect')
-        .attr('class', 'bar')
-        .attr('id', function(d,i){return i;})
-        .attr('x', function(d){ return x(d[0]); })
-        .attr('y', function(d){ return y(d[1]); })
-        .attr('height', function(d){ return height - y(d[1]); })
-        .attr('width', x.rangeBand());
+var bar = chart.selectAll('.bar')
+            .data(info)
+              .enter()
+                .append('rect')
+                .attr('class', 'bar')
+                .attr('id', function(d,i){return i;})
+                .attr('x', function(d){ return x(d[0]); })
+                .attr('y', function(d){ return y(d[1]); })
+                .attr('height', function(d){ return height - y(d[1]); })
+                .attr('width', x.rangeBand());
 
 
 bar.select('text')
@@ -58,3 +61,10 @@ bar.select('text')
     .attr('class', 'y axis')
     .call(yAxis);
 };
+
+
+
+// function update(data){
+//   var text = svg.selectAll("text")
+//               .data(data);
+// }
