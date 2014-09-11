bbc5c7854219637c68885ca0884e95ca70bd67d7
AlisamfP <eatliightning@gmail.com>
2014-09-11 11:55:01 -0700

    moved express folder to root

---
+++ routes/bar_chart.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/bar_chart', function(req, res) {
+  res.render('bar_chart', {title: 'bar chart'});
+});
+
+module.exports = router;
