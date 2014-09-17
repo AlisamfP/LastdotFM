d4f56ff3f5056f40e8ec738c47a7966e6aa64d42
AlisamfP <eatliightning@gmail.com>
2014-09-16 21:27:10 -0700

    using json for data

--- public/javascripts/pie.js
+++ public/javascripts/pie.js
@@ -1,9 +1,10 @@
+var api_url = 'http://ws.audioscrobbler.com/2.0/?method=user.gettopartists&user=alisatrocity&api_key=8148fb40ef9511752203d2c4591e63d0&format=json&period=overall&limit=5';
+
 var width = 960,
     height = 500,
     radius = Math.min(width, height) / 2;
 
-var color = d3.scale.ordinal()
-    .range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"]);
+var color = d3.scale.category20();
 
 var arc = d3.svg.arc()
     .outerRadius(radius - 10)
@@ -13,40 +14,53 @@ var pie = d3.layout.pie()
     .sort(null)
     .value(function(d) { return d.playcount; });
 
-var svg = d3.select("body").append("svg")
-    .attr("width", width)
-    .attr("height", height)
-  .append("g")
-    .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+var svg = d3.select('body').append('svg')
+    .attr('width', width)
+    .attr('height', height)
+  .append('g')
+    .attr('transform', 'translate(' + width / 2 + ',' + height / 2 + ')');
 
-d3.csv("/assets/data.csv", function(error, data) {
+function filter(array){
+  var result = [];
+  for (item in array){
+    result.push({key: item, name: array[item]['name'], playcount: parseInt(array[item]['playcount'])});
+  }
+  console.log(result);
+  return result;
+}
 
-  data.forEach(function(d) {
-    d.playcount = +d.playcount;
-  });
 
-  var g = svg.selectAll(".arc")
+d3.json(api_url, function(error, data) {
+  console.log(data);
+  data = filter(data['topartists']['artist']);
+
+  var g = svg.selectAll('.arc')
       .data(pie(data))
-    .enter().append("g")
-      .attr("class", "arc");
+    .enter().append('g')
+      .attr('class', 'arc');
 
-  g.append("path")
-      .attr("d", arc)
-      .style("fill", function(d) { return color(d.data.name); });
+  g.append('path')
+      .attr('d', arc)
+      .style('fill', function(d) { return color(d.data.name); });
 
-  g.append("text")
-      .attr("transform", function(d) { return "translate(" + arc.centroid(d) + ")"; })
-      .attr("dy", ".35em")
-      .style("text-anchor", "middle")
+  g.append('text')
+      .attr('transform', function(d) { return 'translate(' + arc.centroid(d) + ')'; })
+      .attr('dy', '.35em')
+      .style('text-anchor', 'middle')
+      .style('fill', 'white')
       .text(function(d) { return d.data.name; });
 
   d3.selectAll('.arc').on('mouseover', function (d) {
     var selection = d3.select(this);
-    selection.select('text').text(function (d){ return d.data.playcount; })
+    selection.select('text')
+    .style('font-size', '2em')
+    .text(function (d){ return d.data.playcount; })
   })
   .on('mouseout', function (d){
     var selection = d3.select(this);
-    selection.select('text').text(function (d){ return d.data.name; });
+    selection.select('text')
+    .style('font-size', '1em')
+    .text(function (d){ return d.data.name; });
   })
 
 });
